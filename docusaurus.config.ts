4d052470252cfe6a69fef49db3bb047ede4e01f9
Page Designer <hello@pagedesigner.pk>
2026-05-22 10:24:09 -0400

    Set docs custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://healthyliving-portal-docs.vercel.app',
+  url: 'https://docs.healthyliving.clinic',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
